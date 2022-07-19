e72ba7a5837647d8cb66ad15bfaf82bc3daa3446
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-07-19 13:19:23 -0300

    improvments on home page and logic, bug fixes

---
+++ src/components/home/currentforecast/index.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+
+
+
+
+function ActualWeather(){
+    const [actual, setActual]= useState([])
+
+    async function getData(){
+        let response = await fetch('https://dataservice.accuweather.com/currentconditions/v1/33806?apikey=fLEnBDZUYLr7GMScmfxJrlm01rNUwTmY&language=pt-br&details=true HTTP/1.1')
+        let data = response.json()
+        
+
+        console.log(data)
+        return data
+    }
+     
+    useEffect(()=>{
+
+        getData().then(data => {
+
+            setActual(data[0])
+        })
+            
+        
+
+    },[])
+    console.log(actual)
+        
+    let currentconditions = actual.Temperature.Metric.Value + " °"+ actual.Temperature.Metric.Unit
+    return(
+         <div>   
+            
+                <div className="current-information" style={{display:"flex",
+                                                        justifyContent:"space-around"           }}>
+                <div>
+                    <h3 style={{color:"white",marginBottom:"10px"}}>Juiz de Fora, MG</h3>
+                    <h1 style={{color:"white", fontSize:"60px",paddingTop:"0",marginTop: "0",marginBottom:"0"}}>{currentconditions}</h1>
+                    <h5 style={{color:"white",marginBottom:"10px", marginTop:"0"}}>{actual.WeatherText}</h5>
+                </div>
+
+                <div>
+                    <p style={{fontSize:"10px", marginBottom:"0", marginTop:"20px"}}>Max</p>
+                    <h4 style={{marginTop: "0", paddingTop:"0"}}>0</h4>
+                    <p style={{fontSize:"10px", marginBottom:"0"}}>Min</p>
+                    <h4 style={{marginTop: "0", paddingTop:"0"}}>12°</h4>
+
+                </div>
+        </div>
+    </div>
+    )
+
+} export default ActualWeather
+
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,8 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, {useState } from "react";
 import "./index.css"
 import DailyForecast from "./daylyforecast";
+import ActualWeather from "./currentforecast";
+
 
 const daysForecasts = []
 let daysForecasts3 = []
@@ -17,7 +19,7 @@ function Home (){
     const [daysForecasts3 ,setForecasts3] = useState([])
     
     async function getData(){
-        let response = await fetch ('http://dataservice.accuweather.com/forecasts/v1/daily/5day/33806?apikey=fLEnBDZUYLr7GMScmfxJrlm01rNUwTmY&language=pt-br&details=true&metric=true HTTP/1.1')
+        let response = await fetch ('https://dataservice.accuweather.com/forecasts/v1/daily/5day/33806?apikey=fLEnBDZUYLr7GMScmfxJrlm01rNUwTmY&language=pt-br&details=true&metric=true HTTP/1.1')
         let data = await response.json()
         
         
@@ -41,21 +43,8 @@ function Home (){
                         Bem vindo
                     </h4>
                 </div>
-                <div className="current-information" style={{display:"flex",
-                                                             justifyContent:"space-around"           }}>
-                    <div>
-                        <h3 style={{color:"white",marginBottom:"10px"}}>Juiz de Fora, MG</h3>
-                        <h1 style={{color:"white", fontSize:"60px",paddingTop:"0",marginTop: "0"}}>22°C</h1>
-                    </div>
-
-                    <div>
-                        <p style={{fontSize:"10px", marginBottom:"0", marginTop:"20px"}}>Max</p>
-                        <h4 style={{marginTop: "0", paddingTop:"0"}}>25°</h4>
-                        <p style={{fontSize:"10px", marginBottom:"0"}}>Min</p>
-                        <h4 style={{marginTop: "0", paddingTop:"0"}}>12°</h4>
-
-                    </div>
-                </div>
+                <ActualWeather/>      
+                
                 {daysForecasts3.map((day, index)=> 
                     <DailyForecast 
                     key={index}
